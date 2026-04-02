6c4e4c132c063184e39d1944a8ff702eb6076389
rarufino27-alt <rarufino27@gmail.com>
2026-04-02 08:55:49 -0300

    Update supabase.js

--- core/supabase.js
+++ core/supabase.js
@@ -3,14 +3,10 @@
 const SUPABASE_URL = "https://whzqcvvclmsaskwhzbcw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoenFjdnZjbG1zYXNrd2h6YmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDY2OTEsImV4cCI6MjA5MDY4MjY5MX0.f_RxH38xH0k5UdFnXMLWa66T2fTQ2QhjL3l0qy7ls0Q";
 
-// 🔥 cria UMA vez no escopo global
-window.supabase = window.supabase || window.supabaseClient;
-
+// cria cliente global SEM declarar variável duplicada
 if (!window.supabaseClient) {
   window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
   );
 }
-
-// 👇 NÃO usa const supabase mais
